b64f74c8a015d0b0c0de735923b208d523a10922
lubosfukas <49817123+lubosfukas@users.noreply.github.com>
2022-01-25 17:02:33 +0100

    fix: :ambulance: fix wrong file commited after merge

--- utils/defaults.ts
+++
@@ -1,9 +0,0 @@
-import { CompanionIncome } from '../types'
-
-export const DEFAULT_CHILDREN_ABOVE_SIX = 0
-export const DEFAULT_CHILDREN_BELOW_SIX = 0
-export const DEFAULT_SEVERELY_DISABLED = false
-export const DEFAULT_COMPANION_INCOME: CompanionIncome = {
-    applied: false,
-    income: undefined,
-}
