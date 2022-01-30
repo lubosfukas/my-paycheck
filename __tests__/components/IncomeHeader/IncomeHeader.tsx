c33ed1a02ea1f0ad2d8563700adcc04e3e1c05d9
lubosfukas <49817123+lubosfukas@users.noreply.github.com>
2022-01-30 14:08:38 +0100

    test: :white_check_mark: fix failing test

--- __tests__/components/IncomeHeader/IncomeHeader.tsx
+++ __tests__/components/IncomeHeader/IncomeHeader.tsx
@@ -1,11 +1,11 @@
-import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
 import { IncomeHeader } from '../../../components/IncomeHeader/IncomeHeader'
+import { renderWithContext, screen } from '../../../utils/test'
 
 describe('IncomeHeader', () => {
     test('renders component', () => {
-        render(<IncomeHeader onConfirm={jest.fn()} />)
+        renderWithContext(<IncomeHeader onConfirm={jest.fn()} />)
 
         expect(
             screen.getByRole('heading', {
@@ -34,47 +34,27 @@ describe('IncomeHeader', () => {
 
     test('calls onConfirm', () => {
         const onConfirm = jest.fn()
-        render(<IncomeHeader onConfirm={onConfirm} />)
-
-        const input = screen.getByPlaceholderText(
-            'Zadajte hrubý mesačný príjem (min. 700€)'
-        )
-        expect(input).toBeInTheDocument()
+        renderWithContext(<IncomeHeader onConfirm={onConfirm} />, {
+            monthlyGrossIncome: 2700,
+        })
 
         const calcButton = screen.getByRole('button', { name: 'Vypočítať' })
         expect(calcButton).toBeInTheDocument()
-        expect(calcButton).toBeDisabled()
-
-        userEvent.type(input, '2700')
         expect(calcButton).not.toBeDisabled()
 
         userEvent.click(calcButton)
         expect(onConfirm).toBeCalledTimes(1)
-        expect(onConfirm).toBeCalledWith({
-            monthlyGrossIncome: 2700,
-            monthsWorked: 12,
-            isSeverelyDisabled: false,
-            childrenBelowSix: 0,
-            childrenAboveSix: 0,
-            companionIncome: undefined,
-        })
     })
 
     test('opens modal', () => {
-        render(<IncomeHeader onConfirm={jest.fn()} />)
-
-        const input = screen.getByPlaceholderText(
-            'Zadajte hrubý mesačný príjem (min. 700€)'
-        )
-        expect(input).toBeInTheDocument()
+        renderWithContext(<IncomeHeader onConfirm={jest.fn()} />, {
+            monthlyGrossIncome: 2700,
+        })
 
         const modalButton = screen.getByRole('button', {
             name: 'Rozšírené zadanie',
         })
         expect(modalButton).toBeInTheDocument()
-        expect(modalButton).toBeDisabled()
-
-        userEvent.type(input, '2700')
         expect(modalButton).not.toBeDisabled()
 
         userEvent.click(modalButton)
