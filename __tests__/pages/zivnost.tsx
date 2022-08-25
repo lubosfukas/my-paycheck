8f6f402819a585b61f2a3244d974557e38d4ef73
Lubos Fukas <lubos.fukas@gmail.com>
2022-08-25 16:48:36 +0200

    test: :white_check_mark: cover contract page with unit tests

---
+++ __tests__/pages/zivnost.tsx
@@ -0,0 +1,59 @@
+import { render, screen } from '@testing-library/react'
+
+import Contract from '../../pages/zivnost'
+
+describe('Contract', () => {
+    test('renders page', async () => {
+        render(<Contract />)
+
+        expect(
+            screen.getByRole('heading', {
+                name: 'Živnosť',
+            })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByText(
+                'Tento nástroj vypočítava čistý príjem zo živnosti počas prvého roku aj nasledujúcich rokov.'
+            )
+        ).toBeInTheDocument()
+        expect(
+            screen.getByPlaceholderText('Zadajte mesačný príjem na faktúru')
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('button', { name: 'Vypočítať' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByRole('button', { name: 'Rozšírené zadanie' })
+        ).toBeInTheDocument()
+
+        expect(
+            screen.getByRole('heading', { name: 'Cena práce' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getByText('Priemerný dňový rate v hrubom')
+        ).toBeInTheDocument()
+        expect(screen.getAllByText('0.00€')).toHaveLength(26)
+        expect(
+            screen.getByText('Cena práce na faktúru za rok')
+        ).toBeInTheDocument()
+        expect(screen.getAllByText('Odpracované mesiace')).toHaveLength(3)
+        expect(await screen.findAllByText('12')).toHaveLength(3)
+
+        expect(
+            screen.getByRole('heading', { name: 'Živnosť v prvom roku' })
+        ).toBeInTheDocument()
+        expect(
+            screen.getAllByText('Priemerný čistý mesačný príjem')
+        ).toHaveLength(2)
+        expect(
+            screen.getAllByText('Priemerný čistý ročný príjem')
+        ).toHaveLength(2)
+        expect(
+            screen.getAllByRole('button', { name: 'Odvody a daň' })
+        ).toHaveLength(2)
+
+        expect(
+            screen.getByRole('heading', { name: 'Živnosť po prvom roku' })
+        ).toBeInTheDocument()
+    })
+})
